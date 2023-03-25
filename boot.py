df80a5c976531dc47d6746f8f6ab95532aa997b3
jeznon <118056098+jeznon@users.noreply.github.com>
2023-03-25 17:39:40 -0600

    Add files via upload

---
+++ boot.py
@@ -0,0 +1,31 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+
+try:
+  import usocket as socket
+except:
+  import socket
+
+from machine import Pin
+import network
+
+import esp
+esp.osdebug(None)
+
+import gc
+gc.collect()
+
+ssid = ''
+password = ''
+
+station = network.WLAN(network.STA_IF)
+
+station.active(True)
+station.connect(ssid, password)
+
+while station.isconnected() == False:
+  pass
+
+print('Connection successful')
+print(station.ifconfig())
+
+led = Pin(2, Pin.OUT)
